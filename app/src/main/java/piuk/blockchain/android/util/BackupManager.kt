e127ba4a6ae6f7bf4779c51463c16200e36caf1e
Mats Jerratsch <matsjj@gmail.com>
2020-03-12 11:16:34 +0000

    Only backup specific file

--- app/src/main/java/piuk/blockchain/android/util/BackupManager.kt
+++ app/src/main/java/piuk/blockchain/android/util/BackupManager.kt
@@ -2,14 +2,18 @@ package piuk.blockchain.android.util
 
 import android.app.backup.BackupAgentHelper
 import android.app.backup.SharedPreferencesBackupHelper
+import android.content.Context
 
 class BackupManager: BackupAgentHelper() {
     override fun onCreate() {
-        // The default shared preferences name
-        // https://stackoverflow.com/a/6310080
-        val prefs = packageName + "_preferences"
+        val prefs = SHARED_PREF_NAME
         SharedPreferencesBackupHelper(this, prefs).also {
             addHelper("prefs", it)
         }
+    }
+
+    companion object {
+        // TODO what is a good place for this static? Can't import it from here in coreModule file
+        val SHARED_PREF_NAME = "shared_pref_backup"
     }
 }
