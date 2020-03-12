2ab7d951ceacd7131ef7555edc61c354d5bf63be
Mats Jerratsch <matsjj@gmail.com>
2020-03-12 11:19:10 +0000

    Actually retrieve and push backup values

--- core/src/main/java/piuk/blockchain/androidcore/data/auth/AuthDataManager.kt
+++ core/src/main/java/piuk/blockchain/androidcore/data/auth/AuthDataManager.kt
@@ -156,7 +156,6 @@ class AuthDataManager(
 
     private fun getValidatePinObservable(passedPin: String): Observable<String> {
         val key = prefs.getValue(PersistentPrefs.KEY_PIN_IDENTIFIER, "")
-        val encryptedPassword = prefs.getValue(PersistentPrefs.KEY_ENCRYPTED_PASSWORD, "")
 
         if (!passedPin.isValidPin()) {
             return Observable.error(IllegalArgumentException("Invalid PIN"))
@@ -175,9 +174,9 @@ class AuthDataManager(
                     accessState.isNewlyCreated = false
                     accessState.isRestored = false
                     val decryptionKey = response.body()!!.success
-
+                    handleBackup(decryptionKey)
                     return@map aesUtilWrapper.decrypt(
-                        encryptedPassword,
+                        prefs.getValue(PersistentPrefs.KEY_ENCRYPTED_PASSWORD, ""),
                         decryptionKey,
                         AESUtil.PIN_PBKDF2_ITERATIONS
                     )
@@ -190,6 +189,40 @@ class AuthDataManager(
                     }
                 }
             }
+    }
+
+    /*
+    This function takes care of saving the encrypted values into the special storage for
+    the automatic backup, and also decrypts the values when necessary into local storage.
+     */
+    private fun handleBackup(decryptionKey: String) {
+        if(prefs.hasBackup && prefs.getValue(PersistentPrefs.KEY_WALLET_GUID) == null) {
+            // Pull in the values from the backup, we don't have local state
+            // TODO add restore metric here
+            prefs.setValue(PersistentPrefs.KEY_PIN_IDENTIFIER, prefs.backupPinIdentifier!!)
+            prefs.setValue(PersistentPrefs.KEY_ENCRYPTED_PASSWORD, prefs.backupEncryptedPassword!!)
+            prefs.setValue(PersistentPrefs.KEY_WALLET_GUID, aesUtilWrapper.decrypt(
+                    prefs.backupEncryptedGuid,
+                    decryptionKey,
+                    AESUtil.PIN_PBKDF2_ITERATIONS))
+            prefs.setValue(PersistentPrefs.KEY_SHARED_KEY, aesUtilWrapper.decrypt(
+                    prefs.backupEncryptedSharedKey,
+                    decryptionKey,
+                    AESUtil.PIN_PBKDF2_ITERATIONS))
+        } else {
+            // Make sure to update the backup with up-to-date values.
+            prefs.backupPinIdentifier = prefs.getValue(PersistentPrefs.KEY_PIN_IDENTIFIER, "")
+            prefs.backupEncryptedPassword = prefs.getValue(PersistentPrefs.KEY_ENCRYPTED_PASSWORD, "")
+
+            prefs.backupEncryptedGuid = aesUtilWrapper.encrypt(
+                    prefs.getValue(PersistentPrefs.KEY_WALLET_GUID, ""),
+                    decryptionKey,
+                    AESUtil.PIN_PBKDF2_ITERATIONS)
+            prefs.backupEncryptedSharedKey = aesUtilWrapper.encrypt(
+                    prefs.getValue(PersistentPrefs.KEY_SHARED_KEY, ""),
+                    decryptionKey,
+                    AESUtil.PIN_PBKDF2_ITERATIONS)
+        }
     }
 
     private fun getCreatePinObservable(password: String, passedPin: String): Completable {
