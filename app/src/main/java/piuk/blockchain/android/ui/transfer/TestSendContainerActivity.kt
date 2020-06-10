915b464dfbdac26b970a9f1f6bb6e8e0ec2f7b20
dserrano <dserrano@blockchain.com>
2020-06-10 17:02:40 +0100

    AND-3188 use BaseAuthActivity to prevent from logout timer executing

--- app/src/main/java/piuk/blockchain/android/ui/transfer/TestSendContainerActivity.kt
+++ app/src/main/java/piuk/blockchain/android/ui/transfer/TestSendContainerActivity.kt
@@ -3,11 +3,11 @@ package piuk.blockchain.android.ui.transfer
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import piuk.blockchain.android.R
 import piuk.blockchain.android.ui.transfer.send.TransferSendFragment
+import piuk.blockchain.androidcoreui.ui.base.BaseAuthActivity
 
-class TestSendContainerActivity : AppCompatActivity() {
+class TestSendContainerActivity : BaseAuthActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
