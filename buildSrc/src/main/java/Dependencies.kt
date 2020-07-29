68f6ecea393985a134165a8444f9efab81d4d2b3
dserrano <dserrano@blockchain.com>
2020-07-29 12:14:11 +0100

    AND-3436 revert dependency

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
 
     // Release info
     const val minSdk = 21
-    const val targetSdk = 29
+    const val targetSdk = 28
     const val compileSdk = 28
 
     const val versionCode = 560
