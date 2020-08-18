b56c99d145cbd63183f79b69ffb5cb5e1f28a131
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2020-08-18 19:16:28 +0200

    long-->string (#1210)

--- nabu/src/main/java/com/blockchain/swap/nabu/models/interest/InterestResponses.kt
+++ nabu/src/main/java/com/blockchain/swap/nabu/models/interest/InterestResponses.kt
@@ -1,12 +1,9 @@
 package com.blockchain.swap.nabu.models.interest
 
-import com.squareup.moshi.Json
-
 data class InterestResponse(
     val rate: Double
 )
 
-@Json(name = "BTC")
 data class InterestAccountBalanceResponse(
-    val balance: Long
+    val balance: String
 )
