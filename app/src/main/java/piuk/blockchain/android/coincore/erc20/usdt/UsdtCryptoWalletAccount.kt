2b911e4a9fb85db3d967123c35989e35d0fc01ae
dserrano-bc <64006245+dserrano-bc@users.noreply.github.com>
2020-09-07 17:47:25 +0100

    AND-3594 fix funds for USDT and PAX (#1277)

--- app/src/main/java/piuk/blockchain/android/coincore/erc20/usdt/UsdtCryptoWalletAccount.kt
+++ app/src/main/java/piuk/blockchain/android/coincore/erc20/usdt/UsdtCryptoWalletAccount.kt
@@ -1,6 +1,8 @@
 package piuk.blockchain.android.coincore.erc20.usdt
 
 import info.blockchain.balance.CryptoCurrency
+import info.blockchain.balance.CryptoValue
+import info.blockchain.balance.Money
 import io.reactivex.Single
 import piuk.blockchain.android.coincore.AssetAction
 import piuk.blockchain.android.coincore.ReceiveAddress
@@ -9,6 +11,7 @@ import piuk.blockchain.androidcore.data.erc20.Erc20Account
 import piuk.blockchain.androidcore.data.exchangerate.ExchangeRateDataManager
 import piuk.blockchain.androidcore.data.fees.FeeDataManager
 import piuk.blockchain.androidcore.data.payload.PayloadDataManager
+import java.util.concurrent.atomic.AtomicBoolean
 
 class UsdtCryptoWalletAccount(
     payloadManager: PayloadDataManager,
@@ -24,6 +27,21 @@ class UsdtCryptoWalletAccount(
     label,
     exchangeRates
 ) {
+
+    private val hasFunds = AtomicBoolean(false)
+
+    override val isFunded: Boolean
+        get() = hasFunds.get()
+
+    override val accountBalance: Single<Money>
+        get() = erc20Account.getBalance()
+            .map { CryptoValue.fromMinor(asset, it) }
+            .doOnSuccess {
+                hasFunds.set(it > CryptoValue.ZeroUsdt)
+            }.map {
+                it
+            }
+
     override val receiveAddress: Single<ReceiveAddress>
         get() = Single.just(
             UsdtAddress(address, label)
