1cc72a45ad89c7093360cd890e782b2e25e5d28c
dserrano-bc <64006245+dserrano-bc@users.noreply.github.com>
2020-09-10 17:04:35 +0100

    AND-3639 use display ticker (#1313)

--- app/src/main/java/piuk/blockchain/android/ui/transfer/send/flow/TxConfirmReadOnlyMapper.kt
+++ app/src/main/java/piuk/blockchain/android/ui/transfer/send/flow/TxConfirmReadOnlyMapper.kt
@@ -4,7 +4,6 @@ import android.content.res.Resources
 import info.blockchain.balance.Money
 import piuk.blockchain.android.R
 import piuk.blockchain.android.coincore.TxOptionValue
-import java.lang.IllegalStateException
 
 class TxConfirmReadOnlyMapper(private val formatters: List<TxOptionsFormatter>) {
     fun map(property: TxOptionValue): Pair<String, String> {
@@ -66,9 +65,12 @@ class FeePropertyFormatter(private val resources: Resources) : TxOptionsFormatte
 class ExchangePriceFormatter(private val resources: Resources) : TxOptionsFormatter {
 
     override fun format(property: TxOptionValue): Pair<String, String>? =
-        if (property is TxOptionValue.ExchangePriceOption)
-            resources.getString(R.string.sell_quote_price, property.asset) to property.money.toStringWithSymbol()
-        else null
+        if (property is TxOptionValue.ExchangePriceOption) {
+            resources.getString(R.string.sell_quote_price,
+                property.asset.displayTicker) to property.money.toStringWithSymbol()
+        } else {
+            null
+        }
 }
 
 class TotalFormatter(private val resources: Resources) : TxOptionsFormatter {
