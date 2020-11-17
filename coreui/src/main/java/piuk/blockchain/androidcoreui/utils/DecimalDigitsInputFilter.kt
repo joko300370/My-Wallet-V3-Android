44696dd815e79a3d00f69cfaac1e9b0607d45d2d
Antonis Latas <antonis@blockchain.com>
2020-11-17 14:26:05 +0200

    sync

--- coreui/src/main/java/piuk/blockchain/androidcoreui/utils/DecimalDigitsInputFilter.kt
+++ coreui/src/main/java/piuk/blockchain/androidcoreui/utils/DecimalDigitsInputFilter.kt
@@ -14,7 +14,7 @@ class DecimalDigitsInputFilter(
 
     private val decimalSeparator = DecimalFormatSymbols(Locale.getDefault()).decimalSeparator.toString()
 
-    private val mPattern =
+    private val pattern =
         Pattern.compile("-?[0-9]{0," + digitsBeforeZero + "}+((\\$decimalSeparator[0-9]{0," +
                 digitsAfterZero + "})?)||(\\$decimalSeparator)?")
 
@@ -31,7 +31,7 @@ class DecimalDigitsInputFilter(
 
         val newVal = (dest.subSequence(0, dstart).toString() + replacement +
                 dest.subSequence(dend, dest.length).toString())
-        val matcher = mPattern.matcher(newVal.removePrefix(prefixOrSuffix).removeSuffix(prefixOrSuffix))
+        val matcher = pattern.matcher(newVal.removePrefix(prefixOrSuffix).removeSuffix(prefixOrSuffix))
 
         if (matcher.matches()) return null
         return if (source.isEmpty()) dest.subSequence(dstart, dend) else ""
