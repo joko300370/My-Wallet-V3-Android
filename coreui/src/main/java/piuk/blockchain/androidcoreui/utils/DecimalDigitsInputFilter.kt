8308a3b9ec85db53aa6f57f2995184507f7b2ba4
Antonis Latas <antonis@blockchain.com>
2020-11-17 11:51:38 +0200

    ui

--- coreui/src/main/java/piuk/blockchain/androidcoreui/utils/DecimalDigitsInputFilter.kt
+++ coreui/src/main/java/piuk/blockchain/androidcoreui/utils/DecimalDigitsInputFilter.kt
@@ -2,17 +2,21 @@ package piuk.blockchain.androidcoreui.utils
 
 import android.text.InputFilter
 import android.text.Spanned
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 import java.util.regex.Pattern
 
 class DecimalDigitsInputFilter(
     digitsBeforeZero: Int = 100,
     digitsAfterZero: Int = 100,
     private val prefixOrSuffix: String = ""
-) :
-    InputFilter {
+) : InputFilter {
 
-    private val mPattern = Pattern.compile("-?[0-9]{0," + digitsBeforeZero + "}+((\\.[0-9]{0," +
-            digitsAfterZero + "})?)||(\\.)?")
+    private val decimal = DecimalFormatSymbols(Locale.getDefault()).decimalSeparator.toString()
+    private val thousands = DecimalFormatSymbols(Locale.getDefault()).groupingSeparator.toString()
+
+    private val mPattern =
+        Pattern.compile("^(([0-9]{0,3})*($thousands[0-9]{3})*(\\$decimal[0-9]{0,$digitsAfterZero})?|\\$decimal[0-9]+)$")
 
     override fun filter(
         source: CharSequence,
