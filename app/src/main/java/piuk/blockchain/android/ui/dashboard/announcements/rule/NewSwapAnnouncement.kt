e1f17105bc13f15bc1d528fbdf748dc3e4a5fe8d
Antonis Latas <antonis@blockchain.com>
2020-11-27 18:00:16 +0200

    swap banner

--- app/src/main/java/piuk/blockchain/android/ui/dashboard/announcements/rule/NewSwapAnnouncement.kt
+++ app/src/main/java/piuk/blockchain/android/ui/dashboard/announcements/rule/NewSwapAnnouncement.kt
@@ -4,9 +4,12 @@ import androidx.annotation.VisibleForTesting
 import com.blockchain.swap.nabu.datamanagers.EligibilityProvider
 import com.blockchain.swap.nabu.service.TierService
 import io.reactivex.Single
+import piuk.blockchain.android.R
 import piuk.blockchain.android.ui.dashboard.announcements.AnnouncementHost
 import piuk.blockchain.android.ui.dashboard.announcements.AnnouncementRule
 import piuk.blockchain.android.ui.dashboard.announcements.DismissRecorder
+import piuk.blockchain.android.ui.dashboard.announcements.DismissRule
+import piuk.blockchain.android.ui.dashboard.announcements.StandardAnnouncementCard
 
 class NewSwapAnnouncement(
     private val tiersService: TierService,
@@ -20,7 +23,7 @@ class NewSwapAnnouncement(
         get() = DISMISS_KEY.plus(suggestedAnnouncement.toString())
 
     override val name: String
-        get() = "swap_banners"
+        get() = "swap_v2"
 
     override fun shouldShow(): Single<Boolean> =
         tiersService.tiers().map {
@@ -35,15 +38,34 @@ class NewSwapAnnouncement(
         }.doOnSuccess {
             suggestedAnnouncement = it
         }.map {
-            dismissEntry.isDismissed
+            !dismissEntry.isDismissed
         }
 
     override fun show(host: AnnouncementHost) {
-        println("lalalala $suggestedAnnouncement")
+        host.showAnnouncementCard(
+            card = StandardAnnouncementCard(
+                name = name,
+                dismissRule = DismissRule.CardOneTime,
+                dismissEntry = dismissEntry,
+                titleText = when (suggestedAnnouncement) {
+                    0 -> R.string.airdrop_received_sheet_heading
+                    1 -> R.string.kyc_settings_status_rejected
+                    else -> R.string.kyc_status_title_approved
+                },
+                bodyText = R.string.wdgld_available_card_body,
+                ctaText = R.string.wdgld_available_card_cta,
+                iconImage = R.drawable.vector_dgld_colored,
+                dismissFunction = {
+                    host.dismissAnnouncementCard()
+                },
+                ctaFunction = {
+                    host.dismissAnnouncementCard()
+                }
+            )
+        )
     }
 
     companion object {
-        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-        const val DISMISS_KEY = "SWAP_NEW_ANNOUNCEMENT_DISMISSED"
+        private const val DISMISS_KEY = "SWAP_NEW_ANNOUNCEMENT_DISMISSED"
     }
 }
