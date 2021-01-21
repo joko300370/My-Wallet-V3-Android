7d82c1318897f177f15f16ee629422602b083e56
Antonis Latas <antonis@blockchain.com>
2021-01-21 11:04:41 +0200

    fix proguard coincore

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 29
     const val compileSdk = 28
-    const val versionCode = 600
+    const val versionCode = 601
     const val versionName = "8.4.3"
     const val buildTools = "28.0.3"
 
