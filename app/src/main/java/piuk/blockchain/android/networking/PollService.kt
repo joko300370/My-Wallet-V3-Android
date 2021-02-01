2c6883a6dcb138852b92ad4f4081e600e8b75593
Antonis Latas <antonis@blockchain.com>
2021-02-01 18:57:24 +0200

    poll service

--- app/src/main/java/piuk/blockchain/android/networking/PollService.kt
+++ app/src/main/java/piuk/blockchain/android/networking/PollService.kt
@@ -1,26 +1,30 @@
 package piuk.blockchain.android.networking
 
+import io.reactivex.Flowable
 import io.reactivex.Single
-import java.util.Date
+import io.reactivex.rxkotlin.zipWith
+import java.util.concurrent.TimeUnit
 
-sealed class PollResult<T> {
-    class FinalResult<T>(value: T) : PollResult<T>()
-    class TimeOut<T>(value: T) : PollResult<T>()
-    object Cancel : PollResult<Nothing>()
+sealed class PollResult<T>(val value: T) {
+    class FinalResult<T>(value: T) : PollResult<T>(value)
+    class TimeOut<T>(value: T) : PollResult<T>(value)
 }
 
-class PollService <T>{
+class PollService<T : Any>(
+    private val fetcher: Single<T>,
+    private val matcher: (T) -> Boolean
+) {
 
-   /* private enum ServiceError: Error {
-        case conditionNotMet
-        case pollCancelled
-        case timeout(Value)
-    }*/
-
-   private enum class ServiceError {
-       ConditionNotMet,PollCancelled,TimeOut
-   }
-    private lateinit var endDate: Date
-
-    private lateinit var fetch: (() -> Single<Value>)!
+    fun start(timerInSec: Long = 5, retries: Int = 20) =
+        fetcher.repeatWhen { it.delay(timerInSec, TimeUnit.SECONDS).zipWith(Flowable.range(0, retries)) }
+            .takeUntil {
+                matcher(it)
+            }
+            .lastOrError()
+            .map {
+                if (matcher(it))
+                    PollResult.FinalResult(it)
+                else
+                    PollResult.TimeOut(it)
+            }
 }
