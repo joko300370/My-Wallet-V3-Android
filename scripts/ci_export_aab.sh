0addb195244aa3a71038f7dbd1369c1dfd304374
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-02-10 10:59:22 +0000

    docs(fastlane): AND-4258 Readme Fastlane update (#1743)
    * Updating Readme for fastlane and fixing test commands
    
    * Updating signing commands
    
    * Remove testers group to use default
    
    * Add extra steps for signing

--- scripts/ci_export_aab.sh
+++ scripts/ci_export_aab.sh
@@ -7,10 +7,25 @@ set -x
 aab_path=`echo $1/*.aab | cut -d ' ' -f1`
 
 if [ $2 = true ]; then
-	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $BITRISEIO_ANDROID_KEYSTORE_URL $aab_path "$BITRISEIO_ANDROID_KEYSTORE_ALIAS" -storepass $BITRISEIO_ANDROID_KEYSTORE_PASSWORD -keypass $BITRISEIO_ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD
-fi
 
-cp $aab_path /bitrise/deploy/
+	/usr/bin/jarsigner -sigfile "CERT" -sigalg SHA1withRSA -digestalg SHA1 -keystore $BITRISEIO_ANDROID_KEYSTORE_URL -storepass $BITRISEIO_ANDROID_KEYSTORE_PASSWORD -keypass $BITRISEIO_ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD $aab_path "$BITRISEIO_ANDROID_KEYSTORE_ALIAS"
+
+
+	/usr/bin/jarsigner -verify -verbose -certs $aab_path
+
+	tools_version=`ls /opt/android-sdk-linux/build-tools  | tail -n 1`
+
+	aab_name=`ls -t $aab_path | tail -n 1`
+
+	aligned_aab_path=/bitrise/deploy/aligned/$aab_name
+
+	/opt/android-sdk-linux/build-tools/$tools_version/zipalign -f 4 $aab_path $aligned_aab_path
+
+else
+
+	cp $aab_path /bitrise/deploy/
+
+fi
 
 bitrise_aab_path=`echo /bitrise/deploy/*.aab | cut -d ' ' -f1`
 
