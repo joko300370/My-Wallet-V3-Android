d4996c68f0e94bfddb5bbb0ea821ec4b376a2258
Antonis Latas <antonis@blockchain.com>
2021-02-22 15:59:03 +0200

    formatting

--- sunriver/src/main/kotlin/com/blockchain/sunriver/XlmDataManager.kt
+++ sunriver/src/main/kotlin/com/blockchain/sunriver/XlmDataManager.kt
@@ -172,8 +172,6 @@ class XlmDataManager internal constructor(
     private fun <T> Single<T>.ensureUrlUpdated(): Single<T> =
         xlmProxyUrl.flatMap {
             this
-        }.doOnError {
-            println("error $it")
         }
 
     fun memoToEvent(memo: Memo): CustomEventBuilder = if (memo.isEmpty()) {
