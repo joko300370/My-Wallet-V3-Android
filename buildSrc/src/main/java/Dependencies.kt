d5f9a60fe3f921ef52d71d0c458a1a2e2a461ed6
Antonis Latas <antonis@blockchain.com>
2021-03-09 19:52:40 +0200

    update version name and code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,8 +7,8 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 619
-    const val versionName = "8.5.1"
+    const val versionCode = 620
+    const val versionName = "8.5.2"
     const val buildTools = "29.0.2"
 
     // Build tools and languages
