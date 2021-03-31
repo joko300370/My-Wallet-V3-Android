c032e8e30b2ded040fbd1be8068934fd31c907eb
dtverdota-bc <dtverdota@blockchain.com>
2021-03-31 13:25:06 +0100

    chore(release): AND-0000 version bump: 8.5.5(628)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,8 +7,8 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 627
-    const val versionName = "8.5.4"
+    const val versionCode = 628
+    const val versionName = "8.5.5"
     const val buildTools = "29.0.2"
 
     // Build tools and languages
