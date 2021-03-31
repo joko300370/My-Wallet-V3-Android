4e81333b587739d268b715d49bb98fcf21057756
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-03-31 15:28:17 +0100

    chore(release_version): AND-0000 Bump version (#1871)
    * Fix release build with correct InternalFeatureFlagApi usage
    
    * Undo
    
    * Bump version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 628
+    const val versionCode = 629
     const val versionName = "8.5.5"
     const val buildTools = "29.0.2"
 
