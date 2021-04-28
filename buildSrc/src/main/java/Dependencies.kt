a85d2c6af6b3b630016c28718516525cf882f52c
dserrano <dserrano@blockchain.com>
2021-04-28 15:26:41 +0200

    bump version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 630
+    const val versionCode = 631
     const val versionName = "8.6.0"
     const val buildTools = "29.0.2"
 
