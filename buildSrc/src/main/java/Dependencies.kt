85e74e571a0bffff5abdfb1756464b10dddcbc50
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-06-03 14:45:49 +0100

    fix(custodial_send): AND-4679 Custodial Send Activities (#2012)
    * pending/completed send items fix
    
    * version bump

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 642
+    const val versionCode = 643
     const val versionName = "8.8.0"
     const val buildTools = "29.0.2"
 
